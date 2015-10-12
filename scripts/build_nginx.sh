0c4d1a205cce0d35d7e1167f044d66ad1aa62f43
Mauricio Alvarez <mauricio@curriculet.com>
2015-10-12 16:19:57 -0700

    Updated nginx version to 1.9.5

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -9,7 +9,7 @@
 # Once the dyno has is 'up' you can open your browser and navigate
 # this dyno's directory structure to download the nginx binary.
 
-NGINX_VERSION=${NGINX_VERSION-1.8.0}
+NGINX_VERSION=${NGINX_VERSION-1.9.5}
 PCRE_VERSION=${PCRE_VERSION-8.37}
 ZLIB_VERSION=${ZLIB_VERSION-1.2.8}
 
